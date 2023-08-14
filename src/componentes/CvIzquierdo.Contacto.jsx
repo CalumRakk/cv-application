25e6a8c912f7ad9a2ed8a3c973e62b9299fcae36
Leo <leocasti2@gmail.com>
2023-08-14 09:30:46 -0500

    refactorización: elimina los archivos App.css y index.css

--- src/componentes/CvIzquierdo.Contacto.jsx
+++ src/componentes/CvIzquierdo.Contacto.jsx
@@ -5,11 +5,9 @@ export function Contacto({ telefono, correo }) {
       <h3 className="contacto__titulo">CONTACTO</h3>
       <ul className="contacto_lista">
         <li className="contacto__icon">
-          {" "}
           <img src="src/assets/mail.svg" width="16px" /> {telefono}
         </li>
         <li className="contacto__icon">
-          {" "}
           <img src="src/assets/telefono.svg" width="16px" /> {correo}
         </li>
       </ul>
